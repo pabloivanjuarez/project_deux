8aa8bcb161351e53930615d6e56ae29b9231cd55
pablito <pabloivanjuarez@gmail.com>
2020-10-17 16:23:22 -0700

    using db for .all func

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -3,7 +3,7 @@ var db = require("../models/product");
 var router = express.Router()
 
 router.get("/", function (req, res) {
-  cat.all(function (data) {
+  db.all(function (data) {
     var hbsObject = {
       products: data
     };

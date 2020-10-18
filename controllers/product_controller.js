495a7be877c307e58a3465e206b8ad5d5c6c2431
pablito <pabloivanjuarez@gmail.com>
2020-10-18 14:35:32 -0700

    product controller update

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -1,40 +1,22 @@
 var db = require("../models/product");
 
-<<<<<<< HEAD
-var router = express.Router()
-
-router.get("/", function (req, res) {
-  db.all(function (data) {
-    var hbsObject = {
-      products: data
-    };
-    console.log(hbsObject);
-    res.render("name", hbsObject);
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.render("index");
   });
-});
 
-module.exports = products;
-=======
-module.exports = function(app) {
-    app.get("/", function(req, res) {
-        res.render("index");
+  app.post("/api/products", function (req, res) {
+    db.Product.create(req.body).then(function (dbProduct) {
+      res.json(dbProduct);
+    });
+  });
+
+  app.get("/products", function (req, res) {
+    db.Product.findAll({}).then(function (data) {
+      console.log(data);
+      res.render("productList", {
+        products: data
       });
-
-      app.post("/api/products", function(req, res) {
-        db.Product.create(req.body).then(function(dbProduct) {
-            res.json(dbProduct);
-        });
-      });
-
-      app.get("/products", function(req, res) {
-          db.Product.findAll({
-          }).then(function(data) {
-              console.log(data);
-              res.render("productList", {
-                  products: data
-              });
-          });
-      })
+    });
+  })
 }
-
->>>>>>> b0563c0c27faa58cf59e3a501b464bc0ebd8795f

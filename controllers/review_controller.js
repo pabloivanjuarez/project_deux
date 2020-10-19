2750e8c8bb05dca0d9b5b4d7cd53065413b1b26c
pablito <pabloivanjuarez@gmail.com>
2020-10-19 15:56:53 -0700

    code added for a .get CRUD call

--- controllers/review_controller.js
+++ controllers/review_controller.js
@@ -1,10 +1,18 @@
 var db = require("../models");
+//for a "all reviews page"
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.post("/api/reviews", function(req, res) {
-        db.Review.create(req.body).then(function(dbReview) {
-            res.json(dbReview);
-        });
+  app.get("/api/reviews", (req, res) => {
+    db.reviews.findAll({}).then(function (reviews) {
+      res.render
     })
+  })
+
+
+  app.post("/api/reviews", (req, res) => {
+    db.Review.create(req.body).then(function (dbReview) {
+      res.json(dbReview);
+    });
+  })
 }

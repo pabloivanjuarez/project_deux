c71061f264e00edbb815b778c9062476404947c3
Megan Sleezer <64713309+mslee001@users.noreply.github.com>
2020-10-19 20:46:46 -0700

    Updated the price to match the new DECIMAL format

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,22 @@
 USE copycat;
 
 INSERT INTO products(name, description, price, img, createdAt, updatedAt)
-VALUES ("Gray Kitty", "Beautiful fluffy gray kitten with blue eyes", "$2500", "/assets/images/cat_type_01th.jpg", "2020-10-18","2020-10-18" );
+VALUES ("Gray Kitty", "Beautiful fluffy gray kitten with blue eyes", "2500.00", "/assets/images/cat_type_01th.jpg", "2020-10-18","2020-10-18" );
 
 INSERT INTO products(name, description, price, img, createdAt, updatedAt)
 VALUES ("Orange Kitty", "Gorgeous orange tabby", "$2000", "/assets/images/cat_type_02th.jpg", "2020-10-18","2020-10-18" );
 
 INSERT INTO products(name, description, price, img, createdAt, updatedAt)
-VALUES ("Black Kitty", "Beautiful black kitten. Very friendly.", "$2500", "/assets/images/cat_type_03th.jpg", "2020-10-18","2020-10-18" );
+VALUES ("Black Kitty", "Beautiful black kitten. Very friendly.", "2500.00", "/assets/images/cat_type_03th.jpg", "2020-10-18","2020-10-18" );
 
 INSERT INTO products(name, description, price, img, createdAt, updatedAt)
-VALUES ("Gray & White", "Beautiful gray and white kitten", "$3500", "/assets/images/cat_type_04th.jpg", "2020-10-18","2020-10-18" );
+VALUES ("Gray & White", "Beautiful gray and white kitten", "3500.00", "/assets/images/cat_type_04th.jpg", "2020-10-18","2020-10-18" );
 
 INSERT INTO products(name, description, price, img, createdAt, updatedAt)
-VALUES ("Light Gray Kitty", "Beautiful gray kitten with blue eyes", "$5000", "/assets/images/cat_type_05th.jpg", "2020-10-18","2020-10-18" );
+VALUES ("Light Gray Kitty", "Beautiful gray kitten with blue eyes", "5000.00", "/assets/images/cat_type_05th.jpg", "2020-10-18","2020-10-18" );
 
 INSERT INTO products(name, description, price, img, createdAt, updatedAt)
-VALUES ("Orange & White Kitty", "Beautiful orange and white kitten", "$7500", "/assets/images/cat_type_06th.jpg", "2020-10-18","2020-10-18" );
+VALUES ("Orange & White Kitty", "Beautiful orange and white kitten", "7500.00", "/assets/images/cat_type_06th.jpg", "2020-10-18","2020-10-18" );
 
 INSERT INTO reviews (title, body, createdAt, updatedAt, ProductId)
 VALUES ("Best Kitty", "Very loving", "2020-10-18", "2020-10-18", 1);

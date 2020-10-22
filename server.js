d82ae98dc02e05365f36591c04f84d5df414b19b
pablito <pabloivanjuarez@gmail.com>
2020-10-22 14:43:46 -0700

    requring main controller on server.js

--- server.js
+++ server.js
@@ -5,23 +5,32 @@ const PORT = process.env.PORT || 8080;
 
 const db = require("./models");
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 app.use(express.static("public"));
 
 const Handlebars = require('handlebars');
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const {
+  allowInsecurePrototypeAccess
+} = require('@handlebars/allow-prototype-access');
 const exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+app.engine("handlebars", exphbs({
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
 app.set("view engine", "handlebars");
 
 require("./controllers/product_controller.js")(app);
 require("./controllers/review_controller.js")(app);
 require("./controllers/cart_controller.js")(app);
+require("./controllers/main_controller")(app);
 
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({
+  force: true
+}).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });

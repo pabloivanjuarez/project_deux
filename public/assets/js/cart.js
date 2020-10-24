5c4860861b947f9fabc3660ab8fad2155c3d942a
Megan Sleezer <64713309+mslee001@users.noreply.github.com>
2020-10-23 20:43:13 -0700

    added functionality for total price and grand total

--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -1,4 +1,36 @@
 $(document).ready(function() {
+
+    let prices = [];
+    let quantities = [];
+    let totalPrice = [];
+
+    //gets the price amount for each product and pushes to array
+    $(".product-price").each(function() {
+        let price = $(this).data("price");
+        prices.push(price);
+    })
+
+    //gets the qty for each product and pushes to array
+    $(".itemQty").each(function() {
+        let qty = parseInt($(this).val());
+        quantities.push(qty);
+    })
+
+    //gets total amount (price*qty) for each product and pushes to array
+    for (i=0; i < prices.length; i++) {
+        let total = prices[i] * quantities[i];
+        totalPrice.push(total);
+    }
+
+    //displays the total price for each product in the product table
+    $(".item-price").each(function(i) {
+        $(this).html(`$${totalPrice[i]}.00`);
+    })
+
+    //displays the grand total amount
+    $(".grand-total").html(`$${(totalPrice.reduce((a,b) => a + b, 0))}.00`);
+
+    //submit event when the user updates the qty. The qty updates in the carts table and the page is reloaded
     $(".update-quantity").on("submit", function(event) {
         event.preventDefault();
 
@@ -15,13 +47,13 @@ $(document).ready(function() {
             data: updateQuantity
         }).then(
             function() {
-                console.log(itemId);
-                console.log(itemQty);
                 location.reload();
             }
         );
     });
 
+    //submit event when the user wishes to remove a product from the cart.
+    //The product is removed from the carts table and the page is reloaded.
     $(".delete-item").on("submit", function(event) {
         event.preventDefault();
 
